19a59ce4194a351e710b3e1bfdda1baaf623c8f7
Lasha <gamgoneishvili8@gmail.com>
2025-12-11 06:35:45 +0400

    last updates from youtube lesson

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const profile = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="">profile</Text>
+    <View className="bg-primary flex-1 px-10 ">
+      <View className="flex-1 flex-col items-center justify-center gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-100 text-base">Profile</Text>
+      </View>
     </View>
   );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,14 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const saved = () => {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="">saved</Text>
+    <View className="bg-primary flex-1 px-10 ">
+      <View className="flex-1 flex-col items-center justify-center gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-100 text-base">Save</Text>
+      </View>
     </View>
   );
 };
